cedbcfc19e2f983e35da499a7360dfe626809db0
Samuel <Samuelpmontague@gmail.com>
2021-06-21 17:59:21 -0500

    added script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Input a Bootstrap list with buttons for console logs - completed
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello World!";
+
+}
+
+myFunction();
+// allow button to save input message to local storage  add event listener!]
+    // $('#button-addon2').on('click', function(){
+    //     $('input[type="text"]').each(function(){
+    //         var id = $(this).attr('id');
+    //         var value = $(this).val();
+    //         localStorage.setItem(id, value);
+    //     })
+    // })
+
+
+
+// create function to tell current time
+// color coordinate past, present and future with the current time function
