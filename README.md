715fe5f6b0c31052da6a6b587f113ee1c2f2284a
Samuel <Samuelpmontague@gmail.com>
2021-06-21 22:05:17 -0500

    finalized script.js

---
+++ README.md
@@ -0,0 +1,21 @@
+My objective was to complete a schedule planner for saving local data after user input
+
+Used bootstrap for the forms and used jQuery as much as I could in the Javascript.
+
+
+When initially looking at the header
+the header reads the current day.
+When looking at the hours in the day
+the colors correspond with the current hour.
+When clicking the button "save!"
+the button saves the input locally
+
+
+
+
+my local input was working while I was working with my tutor but now it will only do the 9am save.
+the color input was working as well but it is so late when I am finishing up that I cannot see the colors.
+
+
+![screenshot-top](part1-scheduler.JPG)
+![screenshot-bottom](part2-scheduler.JPG)
--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-// Input a Bootstrap list with buttons for console logs - completed
-function myFunction(){
-    document.getElementById("demo").innerHTML = "Hello World!";
-
-}
-
-myFunction();
-// allow button to save input message to local storage  add event listener!]
-    // $('#button-addon2').on('click', function(){
-    //     $('input[type="text"]').each(function(){
-    //         var id = $(this).attr('id');
-    //         var value = $(this).val();
-    //         localStorage.setItem(id, value);
-    //     })
-    // })
+var date = moment().format('dddd, MMM Do YYYY');
+$('#currentDay').html(date);
 
 
+$(document).ready(function (){
+    $('#button-addon2').on("click", function(){
+        var text = $(this).siblings('.form-control').val();
+        var time = $(this).parent().attr("id")
 
-// create function to tell current time
-// color coordinate past, present and future with the current time function
+        localStorage.setItem(text, time);
+    })
+
+    function currentTime() {
+        var timeNow = moment().hour();
+
+        $(".time-block").each(function (){
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+        
+
+        if (timeBlock < timeNow){
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+
+        else if (timeBlock === timeNow){
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    }
+        
+    $("#9am .form-control").val(localStorage.getItem("9am"));
+    $("#10am .form-control").val(localStorage.getItem("10am"));
+    $("#11am .form-control").val(localStorage.getItem("11am"));
+    $("#12pm .form-control").val(localStorage.getItem("12pm"));
+    $("#1pm .form-control").val(localStorage.getItem("1pm"));
+    $("#2pm .form-control").val(localStorage.getItem("2pm"));
+    $("#3pm .form-control").val(localStorage.getItem("3pm"));
+    $("#4pm .form-control").val(localStorage.getItem("4pm"));
+    $("#5pm .form-control").val(localStorage.getItem("5pm"));
+
+
+
+    currentTime();
+})
+
